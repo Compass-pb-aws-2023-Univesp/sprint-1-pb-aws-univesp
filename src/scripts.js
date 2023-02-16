c94ec2cffa38e9c87aea91c1e4e01f3e56ada0fc
M Vinicios Nativo <onativo@outlook.com>
2023-02-16 15:34:40 -0300

    refactor: troca declaracao variaveis "var -> let" style: organizacao do codigo

--- src/scripts.js
+++ src/scripts.js
@@ -5,11 +5,10 @@ function getEmail(){
   return email
 }
 
-
 //1 - Verificar se o endereço de e-mail é válido usando uma expressão regular.
   //essa função verifica se o uer digitou um email padrão
 function isEmail(email){
-  var emailRegex = new RegExp  (/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+  let emailRegex = new RegExp  (/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
   if(emailRegex.test(email)){
     return true
   }
@@ -18,8 +17,8 @@ function isEmail(email){
 //Verificar se o endereço de e-mail tem pelo menos um nome de usuário e um domínio.
   //essa função checa se o email tem um nome de usuário antes do @ e um domínio depois do @
 function nameDomain(email){
-  var nameRegex = new RegExp (/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))/)
-  var domainRegex = new RegExp (/((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+  let nameRegex = new RegExp (/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))/)
+  let domainRegex = new RegExp (/((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 
   if(nameRegex.test(email) && domainRegex.test(email)){
     return true
@@ -28,7 +27,7 @@ function nameDomain(email){
 
 //função pra verificar se o nome de domínio tem dois ou mais caracteres
 function domainLength(email){
-  lenRegex = new RegExp (/a?[a-zA-Z]{2,}$/)
+  lenRegex = new RegExp (/a?\.[a-zA-Z]{2,}$/)
   var lenString = lenRegex.test(email)
   if(lenString){
     return true
@@ -76,10 +75,10 @@ function dotEDU(email){
   //caso sim, mostra o email digitado com a mensagem "email válido"
     //mostra ainda uma mensagem informando o top-level domain
 function validate(event){
-  var $resultado = $("#resultado")
-  var $result_domain = $("#result_domain")
-  var $result_len = $("#result_len")
-  var email = $("#email").val()
+  let $resultado = $("#resultado")
+  let $result_domain = $("#result_domain")
+  let $result_len = $("#result_len")
+  let email = $("#email").val()
   email = email.toLowerCase()
   $resultado.text("")
   $result_domain.text("")
@@ -90,36 +89,33 @@ function validate(event){
   if(isEmail(email) & nameDomain(email)){
     $resultado.text("O email " + email + " é válido :D")
     $resultado.css("color", "green")
+    //verifica como termina o endereço de email
+    if(dotCOM(email)){
+      $result_domain.text(`O email termina em ".com"`)
+      $result_domain.css("color", "green")}
+    if(dotORG(email)){
+      $result_domain.text(`O email termina em ".org"`)
+      $result_domain.css("color", "green")}
+    if(dotNET(email)){
+      $result_domain.text(`O email termina em ".net"`)
+      $result_domain.css("color", "green")}
+    if(dotBR(email)){
+      $result_domain.text(`O email termina em ".br"`)
+      $result_domain.css("color", "green")}
+    if(dotEDU(email)){
+      $result_domain.text(`O email termina em ".edu"`)
+      $result_domain.css("color", "green")}
   }
   else{
     $resultado.text("O email " + email + " não é válido :/")
     $resultado.css("color", "red")
   }
-  
-  //verifica como termina o endereço de email
-  if(dotCOM(email)){
-    $result_domain.text(`O email termina em ".com"`)
-    $result_domain.css("color", "green")
+  if(!domainLength(email)){
+    $result_len.text("O nome de domínio está errado")
+    $result_len.css("color", "red")
   }
-  if(dotORG(email)){
-    $result_domain.text(`O email termina em ".org"`)
-    $result_domain.css("color", "green")
-  }
-  if(dotNET(email)){
-    $result_domain.text(`O email termina em ".net"`)
-    $result_domain.css("color", "green")
-  }
-  if(dotBR(email)){
-    $result_domain.text(`O email termina em ".br"`)
-    $result_domain.css("color", "green")
-  }
-  if(dotEDU(email)){
-    $result_domain.text(`O email termina em ".edu"`)
-    $result_domain.css("color", "green")
-  }
-  
-  if(domainLength(email)){
-    $result_len.text("O nome de domínio tem mais que 2 letras")
+  else{
+    $result_len.text("O nome de domínio tem 2 ou mais letras")
     $result_len.css("color", "green")
   }
 
