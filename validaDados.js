1e0b133bdb22495fde742af1650052e54cbbb3ac
Bruno Monserrat Perillo <bruno.geofisica@gmail.com>
2023-02-18 20:58:14 -0300

    feat: implementação email.js

--- validaDados.js
+++ validaDados.js
@@ -10,9 +10,15 @@ class validaDados {
   }
 }
 
-console.log(validaDados.validaNome("JOÃO")); // O nome contém somente letras maiúsculas.
-console.log(validaDados.validaNome("maria")); // O nome contém somente letras minúsculas.
-console.log(validaDados.validaNome("Daniel")); // O nome contém caracteres que não são letras ou tem letras maiúsculas e minúsculas.
+do {
+  nome = prompt("Digite o seu nome:");
+  n = validaDados.validaNome(nome);
+} while (n == 0);
+
+
+//console.log(validaDados.validaNome("JOÃO")); // O nome contém somente letras maiúsculas.
+//console.log(validaDados.validaNome("maria")); // O nome contém somente letras minúsculas.
+//console.log(validaDados.validaNome("Daniel")); // O nome contém caracteres que não são letras ou tem letras maiúsculas e minúsculas.
 
 
 //let nome = prompt("Digite o seu nome:");
