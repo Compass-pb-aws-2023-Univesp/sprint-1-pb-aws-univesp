2f04defc3e2d621e2b41b0a882e613c913a2083d
Kelly Lopes <kelypatylop@gmail.com>
2023-02-18 21:45:47 -0300

    código nomeRegex validado

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
+class NomeValidator {
+    static validate(nome) {
+      const nomeRegex = /^[a-zA-Z]+$/;
+      if (nomeRegex.test(nome)) {
+        return nome;
+      } else {
+        throw new Error("Nome inválido! Use apenas letras maiúsculas e minúsculas.");
+      }
+    }
+  }
+
+  const nome = prompt("Digite seu primeiro nome:");
+
 console.log("teste");
