2b7cfa10a2d8ae5c869ef6c7abe7fbab4f55cf81
Carlos Roberto S Camilo <carlos.rs.camilo@unesp.br>
2023-02-19 22:30:37 -0200

    fix: comentários na classe

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
-class validaDados {
+class ValidaDados {
 
+  /* O método validaNome avalia se a string nome contém somente caracteres válidos para um nome de pessoa.
+  Nomes com acento e com o caracter "ç" são aceitos.
+     
+  Se o nome for válido, escreve o nome no console. Caso contrário, retorna uma mensagem de erro.*/
   static validaNome(nome) {
     if (/^[a-zA-ZÀ-ú\sçÇ]+$/.test(nome)) {
         console.log('Nome informado:\n\n'+ nome);
@@ -13,6 +17,10 @@ class validaDados {
     }
   }
 
+  /* O método validaEmail avalia se a string email possui um padrão válido para um endereço de email.
+  São aceitos somente os tipos de domínio .com .edu. .net e .org
+     
+  Se o email for válido, escreve no console somente em letras minúsculas. Caso contrário, retorna uma mensagem de erro.*/
   static validaEmail(email) {
 
     const regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.(com|org|net|br|edu)$/;
@@ -29,6 +37,9 @@ class validaDados {
     }
   }
 
+  /* O método validaTelefone avalia se a string telefone está no formato exigido: (NN)NNNN-NNNN
+     
+  Se o telefone for válido, escreve no console o telefone informado. Caso contrário, retorna uma mensagem de erro.*/
   static validaTelefone(telefone){ 
     
     const regexTelefone = /^\(\d{2}\)\d{4}-\d{4}$/;
@@ -49,15 +60,15 @@ class validaDados {
 
 do {
   nome = prompt("Digite o seu nome:");
-} while (validaDados.validaNome(nome) == 0);
+} while (ValidaDados.validaNome(nome) == 0);
 
 do {
   email = prompt("Digite um endereço de email válido:");
-} while (validaDados.validaEmail(email) == 0);
+} while (ValidaDados.validaEmail(email) == 0);
 
 do {
   telefone = prompt("Digite seu número de telefone no formato (NN)NNNN-NNNN");
-} while (validaDados.validaTelefone(telefone) == 0);
+} while (ValidaDados.validaTelefone(telefone) == 0);
 
 
 console.log('Cadastro finalizado!');
